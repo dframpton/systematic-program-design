af7bef33f4d75ed94091c3b7baf1eafc3b447bac
Dan Frampton <dfrmptn@gmail.com>
2013-10-16 14:09:49 +0100

    Add module 7b lecture files

---
+++ 7b-local/lecture/local-evaluation.rkt
@@ -0,0 +1,39 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname local-evaluation) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+
+;; local-evaluation.rkt
+
+;; Rename -> Lift -> Replace
+
+(define b 1)
+
+(+ b
+   (local [(define b 2)]
+     (* b b))
+   b)
+
+(+ 1
+   (local [(define b 2)]
+     (* b b))
+   b)
+
+(define b_0 2)
+
+(+ 1
+   (* b_0 b_0)
+   b)
+
+(+ 1
+   (* 2 b_0)
+   b)
+
+(+ 1
+   (* 2 2)
+   b)
+
+(+ 1 4 b)
+
+(+ 1 4 1)
+
+6
